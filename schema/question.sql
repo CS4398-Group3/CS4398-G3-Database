bc6eaa8b16d5deab41809c80b40c119d8d42aad5
Nick Gonzalez <ngonza01@live.com>
2020-07-28 16:14:31 -0500

    Added rows to both topics table and question table

--- schema/question.sql
+++ schema/question.sql
@@ -4,3 +4,7 @@ CREATE TABLE question
   questionid int,
   question VARCHAR(2000)
 );
+
+INSERT INTO question (topicid, questionid, question) SELECT 1, 1, 'When was the Declaration of independence signed';
+INSERT INTO question (topicid, questionid, question) SELECT 2, 1, 'What is 10 times 10';
+INSERT INTO question (topicid, questionid, question) SELECT 3, 1, 'What is the formula for area of a rectangular prism';
--- schema/topics.sql
+++ schema/topics.sql
@@ -1,4 +1,10 @@
+DROP TABLE topics;
 CREATE TABLE topics 
 (
-  topicid int
+  topicid int,
+  topicName VARCHAR(50)
 );
+
+INSERT INTO topics (topicid, topicName) SELECT 1, 'History';
+INSERT INTO topics (topicid, topicName) SELECT 2, 'Math';
+INSERT INTO topics (topicid, topicName) SELECT 3, 'Geometry';
