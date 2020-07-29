3acbb589dab5bda3599993e5dbf8d38a8d257261
Nick Gonzalez <ngonza01@live.com>
2020-07-29 13:39:11 -0500

    Added rows to answers corresponding to the questions table

--- schema/answers.sql
+++ schema/answers.sql
@@ -4,3 +4,7 @@ CREATE TABLE answers
   answerid int,
   answer VARCHAR(2000)
 );
+
+INSERT INTO answers (topicid, answerid, answer) SELECT 1, 1, 'Seventeen seventy six';
+INSERT INTO answers (topicid, answerid, answer) SELECT 2, 1, 'One hundred';
+INSERT INTO answers (topicid, answerid, answer) SELECT 3, 1, 'length times width times height';
